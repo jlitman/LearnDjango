31216fa548f0d22500bd76dc5c61360721a92c75
Joe <jlitman@users.noreply.github.com>
2016-03-07 20:12:16 -0500

    Use simple template for details view.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,20 +1,24 @@
 #from django.shortcuts import render
+#once I switch all views to use render I dont need HttpResponse
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list' : latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "po;;s/detail.html",{'question':question})
 
 def results(request, question_id):
     response = "You are looking at the results of question %s."
